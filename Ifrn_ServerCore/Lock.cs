0765987c4479e3ef7c5d18ff03a354a92333fd33
uiwns811 <dltnals5809@gmail.com>
2023-06-06 18:45:37 +0900

    ReaderWriterLock 구현

---
+++ Ifrn_ServerCore/Lock.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ifrn_ServerCore
+{
+    // 재귀적 Lock을 허용할지 (N0)
+    // - Writer Lock을 Acquire한 상태에서 재귀적으로 같은 쓰레드에서 Acquire하는 것을 허용할 것인지
+    // 만약 허용한다면,
+    // 누가 Write를 잡고 있는지 알아야 하므로 WriteThreadId를 기록해준다.
+
+    // 재귀적 Lock 허용
+    // - WriteLock -> WriteLock OK / WriteLock -> ReadLock OK
+    // - 단, ReadLock -> WriteLock X
+
+    // SpinLock : 5000번 -> Yield
+
+    internal class Lock
+    {
+        const int EMPTY_FLAG = 0x00000000;
+        const int WRITE_MASK = 0x7FFF0000;
+        const int READ_MASK = 0x0000FFFF;
+        const int MAX_SPIN_COUNT = 5000;
+
+        // 32bit
+        // [Unused(1)] [WriterThreadId(15)] [ReadCount(16)]
+        int _flag = EMPTY_FLAG;
+        int _writeCount = 0;            // write는 상호배타적이므로 멀티쓰레드에 안전
+
+        public void WriteLock()
+        {
+            // 동일 쓰레드가 WriteLock을 획득하고 있는지
+            int lockThreadId = (_flag & WRITE_MASK) >> 16;
+            if (Thread.CurrentThread.ManagedThreadId == lockThreadId)
+            {
+                _writeCount++;
+                return;
+            }
+
+            // 아무도 WriteLock or ReadLock을 획득하지 않을 때, 경합해서 소유권을 얻는다
+            
+            int desired = (Thread.CurrentThread.ManagedThreadId << 16) & WRITE_MASK;
+            // 현재 쓰레드의 ID를 16비트만큼 밀어서 WRITE_MASK 자리로 옮기고, WRITE_MASK와 AND연산해서 WRITE_MASK를 제외한 위치는 0을 보장한다.
+
+            while(true)
+            {
+                for (int i = 0; i < MAX_SPIN_COUNT; i++)
+                {
+                    if (Interlocked.CompareExchange(ref _flag, desired, EMPTY_FLAG) == EMPTY_FLAG)
+                    {
+                        _writeCount = 1;
+                        return;
+                    }
+                }
+
+                Thread.Yield();
+            }
+        }
+
+        public void WriteUnlock()
+        {
+            int lockCount = --_writeCount;
+            if (lockCount == 0) 
+                 Interlocked.Exchange(ref _flag, EMPTY_FLAG);
+            // _flag를 깨끗한 값으로 밀어준다.
+        }
+
+        public void ReadLock() 
+        {
+            int lockThreadId = (_flag & WRITE_MASK) >> 16;
+            if (Thread.CurrentThread.ManagedThreadId == lockThreadId)
+            {
+                Interlocked.Increment(ref _flag);
+                return;
+            }
+
+            // 아무도 WriteLock을 획득하고 있지 않으면 ReadCount를 1 늘린다
+            while (true)
+            {
+                for (int i =0; i < MAX_SPIN_COUNT; i++)
+                {
+                    int expected = (_flag & READ_MASK);
+                    // 내가 예상하는 값 == WRITE_MASK가 0인 값. 그래서 READ_MASK만 추출된 _flag & READ_MASK가 expected
+                    if (Interlocked.CompareExchange(ref _flag, expected + 1, expected) == expected)
+                        return;
+
+
+
+                    // 쓰레도 A와 B가 동시에 접근하더라도, 결국 먼저 실행되는 애가 있다.
+                    // A가 성공했다 치면, B는 실패할 수 밖에 없다 (두 라인 사이에서 _flag와 expected가 달라짐)
+                    // 그러면 B는 한 spin 더 돌아서 성공하게 될 것이다.
+                }
+
+                Thread.Yield();
+            }
+        }
+
+        public void ReadUnlock()
+        {
+            Interlocked.Decrement(ref _flag);
+            // _flag를 1 줄여줌
+        }
+    }
+}
--- Ifrn_ServerCore/Program.cs
+++ Ifrn_ServerCore/Program.cs
@@ -8,89 +8,38 @@ using System.Threading.Tasks;
 
 namespace Ifrn_ServerCore
 {
-    class Lock
-    {
-        // Auto Reset Event
-        /*
-        // kernel에서는 bool로 다룸
-        AutoResetEvent _available = new AutoResetEvent(true);  
-        // false = 들어올수없음 true = 들어올수있음
-
-        // Auto = 문을 자동으로 닫아준다.
-
-        public void Acquire()
-        {
-            _available.WaitOne();       // 입장 시도
-            //_available.Reset();         // false로 바꿔줌 but WaitOne()에 포함되어 있다.
-        }
-
-        public void Release() 
-        {
-            _available.Set();           // 이벤트의 상태를 바꾼다 -> _available을 true로 바꿔준다.
-        }
-        */
-
-        // Manual Reset Event
-        ManualResetEvent _available = new ManualResetEvent(false);
-      
-        // Acquire에서 WaitOne()과 Reset()이 두 개로 나뉘어짐 -> Lock에서는 사용할 수 없는 방법
-        // 두 라인을 한 번에 실행해야 함 -> AutoResetEvent로 하자
-
-        // 그럼 언제 하냐?
-        // - 한 번에 하나 씩만 입장해야 할 필요가 없는 경우
-        // - 오래 걸리는 작업이 끝났을 때 모든 쓰레드가 재가동해주는 경우
-
-        public void Acquire()
-        {
-            _available.WaitOne();       // 입장 시도
-            _available.Reset();         // false로 바꿔줌
-        }
-
-        public void Release()
-        {
-            _available.Set();           // 이벤트의 상태를 바꾼다 -> _available을 true로 바꿔준다.
-        }
-    }
     internal class Program
     {
-        static int _num = 0;
-        //static Lock _lock = new Lock();
-        static Mutex _lock = new Mutex();
-
-        static void Thread_1()
-        {
-            for (int i = 0; i < 10000; i++)
-            {
-                _lock.WaitOne();
-                //_lock.Acquire();
-                _num++;
-                _lock.ReleaseMutex();
-                //_lock.Release();
-            }
-        }
-
-        static void Thread_2()
-        {
-            for (int i = 0; i < 10000; i++)
-            {
-                _lock.WaitOne();
-                //_lock.Acquire();
-                _num--;
-                _lock.ReleaseMutex();
-                //_lock.Release();
-            }
-
-        }
+        static volatile int count = 0;
+        static Lock _lock = new Lock();
         static void Main(string[] args)
         {
-            Task t1 = new Task(Thread_1);
-            Task t2 = new Task(Thread_2);
+            Task t1 = new Task(delegate ()
+            {
+                for (int i = 0; i < 10000; i++)
+                {
+                    _lock.WriteLock();
+                    count++;
+                    _lock.WriteUnlock();
+                }
+            });
+
+            Task t2 = new Task(delegate ()
+            {
+                for (int i = 0; i < 10000; i++)
+                {
+                    _lock.WriteLock();
+                    count--;
+                    _lock.WriteUnlock();
+                }
+            });
+
             t1.Start();
             t2.Start();
 
             Task.WaitAll(t1, t2);
 
-            Console.WriteLine(_num);
+            Console.WriteLine(count);
         }
     }
 }
