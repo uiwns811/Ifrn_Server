b9ae318ee7ff02767c98c2246753656b8b578413
uiwns811 <dltnals5809@gmail.com>
2023-06-06 19:45:25 +0900

    TLS (Thread Local Storage)

--- Ifrn_ServerCore/Program.cs
+++ Ifrn_ServerCore/Program.cs
@@ -10,37 +10,40 @@ namespace Ifrn_ServerCore
 {
     internal class Program
     {
-        static volatile int count = 0;
-        static Lock _lock = new Lock();
+        static ThreadLocal<string> ThreadName = new ThreadLocal<string>(() => { return $"My Name Is {Thread.CurrentThread.ManagedThreadId}"; });  
+        // 전역 : 모든 쓰레드가 공유
+        // ThreadLocal<> : 쓰레드마다 접근하면 자신만의 공간에 저장됨
+        
+        static void WhoAmI()
+        {
+            bool repeat = ThreadName.IsValueCreated;
+            if (repeat)
+                Console.WriteLine(ThreadName.Value + "  (repeat)");
+            else
+                Console.WriteLine(ThreadName.Value);
+
+            // 이러면 ThreadName.Value == null일때 (repeat == false일 때)
+            // 생성자에 넣은 람다함수가 호출됨
+
+            // ThreadLocal은 대부분 static에 넣는다.
+
+        }
+
         static void Main(string[] args)
         {
-            Task t1 = new Task(delegate ()
-            {
-                for (int i = 0; i < 10000; i++)
-                {
-                    _lock.WriteLock();
-                    count++;
-                    _lock.WriteUnlock();
-                }
-            });
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(3, 3);
+            Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
 
-            Task t2 = new Task(delegate ()
-            {
-                for (int i = 0; i < 10000; i++)
-                {
-                    _lock.WriteLock();
-                    count--;
-                    _lock.WriteUnlock();
-                }
-            });
-
-            t1.Start();
-            t2.Start();
-
-            Task.WaitAll(t1, t2);
-
-            Console.WriteLine(count);
+            ThreadName.Dispose();
+            // 필요없을 때 날려준다.
         }
     }
 }
 
+// 응용 방법
+// - Job Queue에서 여러 개를 가져와, 자기만의 공간(TLS)에 넣어둔 뒤
+//   Lock을 걸지 않고 하나씩 꺼내서 사용한다.
+
+// 공용 공간 접근 횟수를 줄일 수 있다.
+ 
