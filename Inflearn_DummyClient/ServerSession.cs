7e082d077e3466c73aa04de7a5786dbbf51c9c8e
uiwns811 <dltnals5809@gmail.com>
2023-06-08 23:52:21 +0900

    Serialization

--- Inflearn_DummyClient/ServerSession.cs
+++ Inflearn_DummyClient/ServerSession.cs
@@ -10,22 +10,68 @@ using System.Threading.Tasks;
 
 namespace Inflearn_DummyClient
 {
-    class Packet
+    abstract class Packet
     {
         public ushort size;
         public ushort packetId;
+
+        public abstract ArraySegment<byte> Write();
+        public abstract void Read(ArraySegment<byte> s);
     }
 
     class PlayerInfoReq : Packet
     {
         public long playerId;
+
+        public PlayerInfoReq()
+        {
+            this.packetId = (ushort)PacketID.PlayerInfoReq;
+
+        }
+
+        public override void Read(ArraySegment<byte> s)
+        {
+            // 역직렬화 : 버퍼에 있는거 꺼내쓰기
+            ushort count = 0;
+            //ushort size = BitConverter.ToUInt16(s.Array, s.Offset);
+            count += 2;
+            // ushort id = BitConverter.ToUInt16(s.Array, s.Offset + count);
+            count += 2;
+            // count가 버퍼의 크기를 벗어나진 않았는지 지속적으로 체크해줘야 함 
+
+            this.playerId = BitConverter.ToUInt16(new ReadOnlySpan<byte>(s.Array, s.Offset + count, s.Count - count));
+            // this.playerId = BitConverter.ToUInt16(s.Array, s.Offset + count);       // 충분한 공간이 있는지 체크 필요
+            // - 범위를 정해서 넣어줄 수 있는 위 버전 (ReadOnlySpan)이 훨씬 안전하다.
+            count += 8;
+        }
+
+        public override ArraySegment<byte> Write()
+        {
+            ArraySegment<byte> s = SendBufferHelper.Open(4096);
+
+            ushort count = 0;
+            bool success = true;
+
+            // - success와 & 연산을 통해 TryWriteBytes가 한 번이라도 실패하면 success = false;
+            // - 실패하는 경우 : s가 2byte짜린데 우리가 8byte packet을 넣어준 경우 등 .. 
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), this.packetId);
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), this.playerId);
+            count += 8;
+
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count), count);
+            // 실제 count하기 전에는 packet.size를 알 수 없음. 그래서 패킷을 다 채우고 난 뒤에 맨 앞에다가 count만큼 넣어주자
+            // - but 패킷 헤더의 값은 체크용이다
+
+            if (success == false)
+                return null;
+
+            return SendBufferHelper.Close(count);
+
+        }
     }
 
-    class PlayerInfoOK : Packet
-    {
-        public int hp;
-        public int attack;
-    }
 
     public enum PacketID
     {
@@ -35,32 +81,24 @@ namespace Inflearn_DummyClient
 
     class ServerSession : Session
     {
+        // unsafe : C++처럼 포인터로 조작 가능
+        static unsafe void ToBytes(byte[] array, int offset, ulong value)
+        {
+            fixed (byte* ptr = &array[offset])
+                *(ulong*)ptr = value;
+        }
+
         public override void OnConnected(EndPoint endPoint)
         {
             Console.WriteLine($"OnConnected : {endPoint}");
 
-            PlayerInfoReq packet = new PlayerInfoReq() { size = 4, packetId = (ushort)PacketID.PlayerInfoOK, playerId = 1001 };
+            PlayerInfoReq packet = new PlayerInfoReq() { playerId = 1001 };
 
-            //for (int i = 0; i < 5; i++)
-            {
-                ArraySegment<byte> s = SendBufferHelper.Open(4096);
-
-
-                byte[] size = BitConverter.GetBytes(packet.size);                   // 2
-                byte[] packetId = BitConverter.GetBytes(packet.packetId);           // 2
-                byte[] playerId = BitConverter.GetBytes(packet.playerId);           // 8
-
-                ushort count = 0;
-                Array.Copy(size, 0, s.Array, s.Offset + count, 2);
-                count += 2;
-                Array.Copy(packetId, 0, s.Array, s.Offset + count, 2);
-                count += 2;
-                Array.Copy(playerId, 0, s.Array, s.Offset + count, 8);
-                count += 8;
-                ArraySegment<byte> sendBuff = SendBufferHelper.Close(count);
-
-                Send(sendBuff);
-            }
+            // 직렬화 : 버퍼에 우겨넣기
+            ArraySegment<byte> s = packet.Write();
+            
+            if (s != null)
+                Send(s);
         }
 
         public override void OnDisconnected(EndPoint endPoint)
--- Inflearn_Server/ClientSession.cs
+++ Inflearn_Server/ClientSession.cs
@@ -9,22 +9,65 @@ using System.Threading;
 using System.Threading.Tasks;
 
 namespace Inflearn_Server
-{
-    class Packet
+{    abstract class Packet
     {
         public ushort size;
         public ushort packetId;
-    }
 
+        public abstract ArraySegment<byte> Write();
+        public abstract void Read(ArraySegment<byte> s);
+    }
+ 
     class PlayerInfoReq : Packet
     {
         public long playerId;
-    }
 
-    class PlayerInfoOK : Packet
-    {
-        public int hp;
-        public int attack;
+        public PlayerInfoReq()
+        {
+            this.packetId = (ushort)PacketID.PlayerInfoReq;
+
+        }
+
+        public override void Read(ArraySegment<byte> s)
+        {
+            // 역직렬화 : 버퍼에 있는거 꺼내쓰기
+            ushort count = 0;
+            //ushort size = BitConverter.ToUInt16(s.Array, s.Offset);
+            count += 2;
+            // ushort id = BitConverter.ToUInt16(s.Array, s.Offset + count);
+            count += 2;
+            // count가 버퍼의 크기를 벗어나진 않았는지 지속적으로 체크해줘야 함 
+
+            this.playerId = BitConverter.ToUInt16(new ReadOnlySpan<byte>(s.Array, s.Offset + count, s.Count - count));
+            // this.playerId = BitConverter.ToUInt16(s.Array, s.Offset + count);       // 충분한 공간이 있는지 체크 필요
+            // - 범위를 정해서 넣어줄 수 있는 위 버전 (ReadOnlySpan)이 훨씬 안전하다.
+            count += 8;
+        }
+
+        public override ArraySegment<byte> Write()
+        {
+            ArraySegment<byte> s = SendBufferHelper.Open(4096);
+
+            ushort count = 0;
+            bool success = true;
+
+            // - success와 & 연산을 통해 TryWriteBytes가 한 번이라도 실패하면 success = false;
+            // - 실패하는 경우 : s가 2byte짜린데 우리가 8byte packet을 넣어준 경우 등 .. 
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), this.packetId);
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), this.playerId);
+            count += 8;
+
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count), count);
+            // 실제 count하기 전에는 packet.size를 알 수 없음. 그래서 패킷을 다 채우고 난 뒤에 맨 앞에다가 count만큼 넣어주자
+
+            if (success == false)
+                return null;
+
+            return SendBufferHelper.Close(count);
+
+        }
     }
 
     public enum PacketID
@@ -51,19 +94,21 @@ namespace Inflearn_Server
 
         public override void OnRecvPacket(ArraySegment<byte> buffer)
         {
+            // 역직렬화 : 버퍼에 있는거 꺼내쓰기
             ushort count = 0;
             ushort size = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
             count += 2;
             ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + count);
             count += 2;
+            // count가 버퍼의 크기를 벗어나진 않았는지 지속적으로 체크해줘야 함 
 
             switch ((PacketID)id)
             {
                 case PacketID.PlayerInfoReq:
                     {
-                        long playerId = BitConverter.ToUInt16(buffer.Array, buffer.Offset + count);
-                        count += 8;
-                        Console.WriteLine($"PlayerInfoReq ID : {playerId}");
+                        PlayerInfoReq p = new PlayerInfoReq();
+                        p.Read(buffer);
+                        Console.WriteLine($"PlayerInfoReq ID : {p.playerId}");
                     }
                     break;
                 case PacketID.PlayerInfoOK:
